b9cfb626e9e7038eb2bcfbceabdfee5a2fcefbb0
Titouan <titouanphilippe57@gmail.com>
2026-03-03 17:24:14 +0000

    UpdateV1

--- Library.MVC/Data/ApplicationDbContext.cs
+++ Library.MVC/Data/ApplicationDbContext.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Library.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Library.MVC.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
+
+        
     }
+
+    public DbSet<Product> Products { get; set; }
 }
